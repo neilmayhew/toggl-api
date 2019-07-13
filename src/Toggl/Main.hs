e72c503ef391cff579355cb864269bbe91a34b40
Neil Mayhew <neil@neil.mayhew.name>
2019-07-13 13:17:31 -0600

    Initial commit

---
+++ src/Toggl/Main.hs
@@ -0,0 +1,25 @@
+module Toggl.Main where
+
+import Control.Monad (unless)
+import Data.Aeson (eitherDecodeFileStrict)
+import Data.Bitraversable (bitraverse)
+import Data.Either (partitionEithers)
+import Data.Foldable (traverse_)
+import System.IO (hPutStrLn, stderr)
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import Toggl.Types (TimeEntry)
+
+handleFiles :: ([TimeEntry] -> IO a) -> [FilePath] -> IO ([String], a)
+handleFiles action files =
+    bitraverse pure (action . concat) . partitionEithers =<<
+    traverse eitherDecodeFileStrict files
+
+mainFilesWith :: ([TimeEntry] -> IO Bool) -> [FilePath] -> IO ()
+mainFilesWith action files = do
+    (errs, success) <- handleFiles action files
+    traverse_ (hPutStrLn stderr) errs
+    unless (null errs && success) exitFailure
+
+mainWith :: ([TimeEntry] -> IO Bool) -> IO ()
+mainWith action = mainFilesWith action =<< getArgs
---
+++ src/Toggl/Types.hs
@@ -0,0 +1,93 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Toggl.Types where
+
+import Data.Aeson (FromJSON(..), ToJSON(..), (.:), (.=), object, withObject)
+import Data.Fixed (Centi)
+import Data.Text (Text)
+import Data.Time (UTCTime, NominalDiffTime)
+import GHC.Generics (Generic)
+
+type Money = Centi
+
+data TimeEntry = TimeEntry
+    { teId :: Int
+
+    , teUid :: Int
+    , teUser :: Text
+
+    , teTid :: Maybe Int
+    , teTask :: Maybe Text
+
+    , tePid :: Int
+    , teProject :: Text
+
+    -- Not documented
+    , teProjectColor :: Text
+    , teProjectHexColor :: Text
+
+    , teClient :: Text
+
+    , teDescription :: Text
+
+    , teStart :: UTCTime
+    , teEnd :: UTCTime
+    , teUpdated :: UTCTime
+    , teDur :: NominalDiffTime
+    , teUseStop :: Bool
+
+    , teIsBillable :: Bool
+    , teBillable :: Maybe Money
+    , teCur :: Maybe Text
+
+    , teTags :: [Text]
+    } deriving (Eq, Show, Generic)
+
+instance FromJSON TimeEntry where
+    parseJSON = withObject "TimeEntry" $ \o -> TimeEntry
+        <$> o .: "id"
+        <*> o .: "uid"
+        <*> o .: "user"
+        <*> o .: "tid"
+        <*> o .: "task"
+        <*> o .: "pid"
+        <*> o .: "project"
+        <*> o .: "project_color"
+        <*> o .: "project_hex_color"
+        <*> o .: "client"
+        <*> o .: "description"
+        <*> o .: "start"
+        <*> o .: "end"
+        <*> o .: "updated"
+        <*> ((/1000) <$> o .: "dur") -- Convert from milliseconds
+        <*> o .: "use_stop"
+        <*> o .: "is_billable"
+        <*> o .: "billable"
+        <*> o .: "cur"
+        <*> o .: "tags"
+
+instance ToJSON TimeEntry where
+    toJSON TimeEntry{..} = object
+        [ "id" .= teId
+        , "uid" .= teUid
+        , "user" .= teUser
+        , "tid" .= teTid
+        , "task" .= teTask
+        , "pid" .= tePid
+        , "project" .= teProject
+        , "project_color" .= teProjectColor
+        , "project_hex_color" .= teProjectHexColor
+        , "client" .= teClient
+        , "description" .= teDescription
+        , "start" .= teStart
+        , "end" .= teEnd
+        , "updated" .= teUpdated
+        , "dur" .= (round $ teDur * 1000 :: Integer) -- Convert to milliseconds
+        , "use_stop" .= teUseStop
+        , "is_billable" .= teIsBillable
+        , "billable" .= teBillable
+        , "cur" .= teCur
+        , "tags" .= teTags
+        ]
